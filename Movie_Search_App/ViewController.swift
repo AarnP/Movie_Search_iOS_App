5ca38499f0cef095930c9f4f4c0c0fe2342293be
Aaron Phan <aarn.phan@gmail.com>
2023-06-23 14:25:47 +1000

    Initial Commit

---
+++ Movie_Search_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_Search_App
+//
+//  Created by Aaron Phan on 23/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
